ff6dc04337d2cd2dc4e83dc1a75a763ec76d5b67
Atik Sarker <atikbm7@gmail.com>
2019-07-26 09:44:56 +0600

    create cart.vue, inventory.vue, navbar.vue and add components App.vue file. moreover, create json data store all data into the data.js file, then showing all data on the inventory components

---
+++ src/components/cart.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul class="list-group">
+        <li class="list-group-item">Item - Price</li>
+        <li class="list-group-item">Item - Price</li>
+        <li class="list-group-item">Item - Price</li>
+    </ul>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/inventory.vue
@@ -0,0 +1,27 @@
+<template>
+
+        <div class="row">
+            <div class="col-lg-3"  v-for="(item , index) in items" :key="index">
+                <div class="card">
+                    <img class="card-img-top" v-bind:src="item.image" v-bind:alt="item.name">
+                    <div class="card-body">
+                        <h5 class="card-title">{{item.name}}</h5>
+                        <p class="card-text">{{item.price}}</p>
+                        <a href="#" class="btn btn-info">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</template>
+
+<script>
+    export default {
+        props : ['items']
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
