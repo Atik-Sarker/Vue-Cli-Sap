c8a52505fefe4b491ae9485a0dc3e4df43524378
=Atik-Sarker <atikbm7@gmail.com>
2019-07-29 15:34:04 +0600

    install axios and setup axios and fatch data from api then show data on the inventory

--- src/components/view/inventory.vue
+++ src/components/view/inventory.vue
@@ -1,6 +1,6 @@
 <template>
 
-        <div class="row">
+        <div class="row" v-if="! lodding">
             <div class="col-lg-3"  v-for="(item , index) in items" :key="index">
                 <div class="card">
                     <img class="card-img-top" v-bind:src="item.image" v-bind:alt="item.name">
@@ -11,23 +11,43 @@
                     </div>
                 </div>
             </div>
-        </div>
 
+        </div>
+    <h1 v-else>Lodding.....</h1>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
         // received data from parent component using props
         // props : ['items'],
         data(){
           return {
+              lodding: true,
               items: []
           }
+        },
+        mounted(){
+            this.fatchInventory()
         },
         methods:{
             // send event chaild to parent component
             addToCart(item){
                 return this.$emit('newItemAdded', item)
+            },
+            fatchInventory(){
+                var self = this;
+                axios.get('http://localhost:3000/items').then(response =>{
+
+                        self.lodding = false;
+                        self.items = response.data;
+
+                    // setTimeout(function(){
+                    //     self.lodding = false;
+                    //     self.items = response.data;
+                    // }, 3000);
+
+                })
             }
         }
     }
